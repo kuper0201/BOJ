acd3ba20294e452abf8cff682eaef4374b3a1a9a
kuper0201 <kuper171@gmail.com>
2022-08-10 14:24:24 +0900

    Create BOJ_17608.java
    https://boj.kr/17608
    막대기 문제 풀이

---
+++ BOJ_17608.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stk = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) stk.push(Integer.parseInt(br.readLine()));
+
+        int max = Integer.MIN_VALUE, cnt = 0;
+        while(!stk.empty()) {
+            int pop = stk.pop();
+            if(pop > max) {
+                max = pop;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
