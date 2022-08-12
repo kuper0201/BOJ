be1be972c896747edca55c152874dc40a470f72f
kuper0201 <kuper171@gmail.com>
2022-08-12 13:48:46 +0900

    Create BOJ_10821.java

---
+++ BOJ_10821.java
@@ -0,0 +1,15 @@
+/*
+2022.08.12. (금)
+https://boj.kr/10821
+"막대기" 문제 풀이
+*/
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(",");
+        System.out.println(str.length);
+    }
+}
